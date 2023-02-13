03c9a461cbc36f98d5b2ba713189df0ec285bb88
Nathan LeRoy <NLeRoy917@gmail.com>
2023-02-13 16:31:16 -0500

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import React from 'react'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +14,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KJCH91XHWJ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KJCH91XHWJ');`,
+          }}
+        />
         <Component {...pageProps} />
       </Hydrate>
     </QueryClientProvider>
