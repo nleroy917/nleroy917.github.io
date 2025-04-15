530045513af8f5a72fdd9fbff81c68ed4d83e171
Nathan LeRoy <NLeRoy917@gmail.com>
2025-04-15 19:13:10 -0400

    update code logic

--- pages/hunt.tsx
+++ pages/hunt.tsx
@@ -1,17 +1,16 @@
 import { NextPage } from 'next'
 import { Fragment, useState } from 'react'
 
-const REAL_CODE = 123
-
 const Hunt: NextPage = () => {
   const [code, setCode] = useState('')
   const [unlocked, setUnlocked] = useState(false)
 
   const handleCodeSubmit = () => {
-    if (parseInt(code) === REAL_CODE) {
+    // just take if the code is 4 digits long
+    if (code.length === 4) {
       setUnlocked(true)
     } else {
-      alert('Incorrect code. Try again.')
+      alert('Please enter a valid code')
     }
   }
 
@@ -21,14 +20,14 @@ const Hunt: NextPage = () => {
         <p className="w-full text-2xl lg:w-94">
           Your next clue… Will be found near a monarch’s frozen delight There,
           we sat uncertain on a Friday night. With spoons in hand, adoption
-          forms galore— We expanded our ”family” from 3 to four.
+          forms galore— We expanded our ”family” from 3 to 4.
         </p>
         <div className="flex flex-row items-center justify-center space-x-2">
           <input
             onChange={(e) => setCode(e.target.value)}
             value={code}
             type="text"
-            placeholder="Got a code?"
+            placeholder="Got a code? Not yet? Just ignore :)"
             className="w-full p-2 border-2 border-gray-300 rounded-md lg:w-96"
           />
           <button
